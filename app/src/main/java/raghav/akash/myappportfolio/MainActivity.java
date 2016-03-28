9179b05ca2042537b8107ca695472f4f940c5ece
Akash <akash.raghav@aspiringminds.in>
2016-03-28 12:21:34 +0530

    Ui color and theme changed

--- app/src/main/java/raghav/akash/myappportfolio/MainActivity.java
+++ app/src/main/java/raghav/akash/myappportfolio/MainActivity.java
@@ -1,13 +1,26 @@
 package raghav.akash.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().setBackgroundDrawable(null);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    toolbar.setTitle("My App Portfolio");
+    setSupportActionBar(toolbar);
+  }
+
+  public void showMessageToast(View v) {
+    Button button = (Button) v;
+    Toast.makeText(this, button.getText() + " app Portfolio", Toast.LENGTH_SHORT).show();
+  }
 }
